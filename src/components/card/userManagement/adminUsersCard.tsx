ec33b05d436e9bd934d04b48f265e5911f2cbb03
SharmeenFatima-11 <sharmeen.fatima@webrangesolutions.com>
2025-12-04 16:12:25 +0500

    miles and movement types

--- src/components/card/userManagement/adminUsersCard.tsx
+++ src/components/card/userManagement/adminUsersCard.tsx
@@ -40,7 +40,7 @@ const AdminUserCard: React.FC<AdminUserCardProps> = ({
       {isAdmin == true ? (
         <div className="flex flex-col text-left w-full sm:w-auto">
           <span className="font-bold text-lg sm:text-xl">Admin users</span>
-          <span className="text-sm sm:text-base">
+          <span className="text-sm sm:text-base whitespace-nowrap">
             Admin users can add and remove users and <br></br> manage
             organization-level settings.
           </span>
@@ -48,7 +48,7 @@ const AdminUserCard: React.FC<AdminUserCardProps> = ({
       ) : (
         <div className="flex flex-col text-left w-full sm:w-auto">
           <span className="font-bold text-lg sm:text-xl">View Only</span>
-          <span className="text-sm sm:text-base">
+          <span className="text-sm sm:text-base whitespace-nowrap">
             View only users can view all content but may <br></br> not make any
             changes.
           </span>
